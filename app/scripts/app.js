ebfcb93d616cd8bdf687eea760b56b60dbf8d8c2
Øyvind Hellenes <hellenes.91@gmail.com>
2015-02-15 17:49:13 +0100

    crud init

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -5,4 +5,12 @@ angular
     'ngCookies',
     'ngResource',
     'ngSanitize'
-  ]);
+  ])
+  .config(['$httpProvider','$resourceProvider', function($httpProvider, $resourceProvider){
+  	$httpProvider.defaults.xsrfCoockieName = 'csrftoken';
+  	$httpProvider.defaults.xsrfHeaderName = 'X-CSRFToken';
+
+  	// Don't strip trailing slashes from calculated URLs
+  	$resourceProvider.defaults.stripTrailingSlashes = false;
+
+  }])
